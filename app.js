96a1be18c0eb40714cc0f97beb66f42b15b6b9e8
Akash0000001 <akashranjan947@gmail.com>
2023-05-26 02:00:47 +0530

    sending responses

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const http=require("http")
 const server=http.createServer((req,res)=>{
-    console.log("My Name is Akash Ranjan")
+    res.setHeader("Content-Type","text/html");
+    res.write("<html>")
+    res.write("<head><title>Homepage</title></head>")
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>")
+    res.write("</html>")
+    res.end()
 })
 server.listen(4000)
