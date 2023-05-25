348809bbe4d64b7a6c4dd557cb9cc0f75e62d23c
Akash0000001 <akashranjan947@gmail.com>
2023-05-26 02:19:52 +0530

    sending response by using url

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const http=require("http")
 const server=http.createServer((req,res)=>{
-    res.setHeader("Content-Type","text/html");
-    res.write("<html>")
-    res.write("<head><title>Homepage</title></head>")
-    res.write("<body><h1>Welcome to my Node Js project</h1></body>")
-    res.write("</html>")
-    res.end()
+    const url=req.url
+    if(url==="/Home")
+    {
+        res.setHeader("Content-Type","text/html");
+        res.write("<html>")
+        res.write("<head><title>Homepage</title></head>")
+        res.write("<body><h1>Welcome home</h1></body>")
+        res.write("</html>")
+        res.end()
+    }
+    else if(url==="/about")
+    {
+        res.setHeader("Content-Type","text/html");
+        res.write("<html>")
+        res.write("<head><title>About us</title></head>")
+        res.write("<body><h1>Welcome to About Us page</h1></body>")
+        res.write("</html>")
+        res.end()
+    }
+    else if(url==="/node")
+    {
+        res.setHeader("Content-Type","text/html");
+        res.write("<html>")
+        res.write("<head><title>Project</title></head>")
+        res.write("<body><h1>Welcome to Node Js project</h1></body>")
+        res.write("</html>")
+        res.end() 
+    }
+
 })
 server.listen(4000)
