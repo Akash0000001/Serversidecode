eeb30c706e046f9f6fedd6f7b764336b4f52dc4b
Akash0000001 <akashranjan947@gmail.com>
2023-05-27 03:05:26 +0530

    reading and writing data

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const http=require("http")
 const fs=require("fs")
+
+
+
 const server=http.createServer((req,res)=>{
     const url=req.url
     if(url==="/")
     {
+        return fs.readFile("message.txt",{"encoding":"utf-8"},(err,data)=>{
+        if (err)
+        {
+            console.log(err)
+        }
+        else{
         res.setHeader("Content-Type","text/html");
         res.write("<html>")
         res.write("<head><title>Message</title></head>")
         res.write("<body>")
-        res.write("<h1>Enter Message:</h1>")
+        res.write(`<p>${data}`)
         res.write("<form action=/message method='POST'><input type='text' name='message'></input><input type='submit' value='Send'></input></form>")
         res.write("</body>")
         res.write("</html>")
         return res.end()
+        }
+    })
     }
     else if(url==="/message")
-    {
-        fs.writeFileSync("message.txt","Dummy");
-        res.statusCode=302;
-        res.setHeader("location","/")
-        return res.end();
+    {   const body=[];
+        req.on("data",chunk=>{
+        body.push(chunk)
+    })
+        return req.on("end",()=>{
+            fs.writeFile("message.txt",Buffer.concat(body).toString().split("=")[1],err=>{
+                if(err)
+                {
+                    console.log(err)
+                }
+                else{
+                res.statusCode=302;
+                res.setHeader("location","/")
+                return res.end();
+                }
+            });
+        })
+        
+        
     }
     else if(url==="/about")
     {
@@ -39,13 +64,14 @@ const server=http.createServer((req,res)=>{
         res.write("</html>")
         return res.end() 
     }
+    
     res.setHeader("Content-Type","text/html");
     res.write("<html>")
     res.write("<head><title>Project</title></head>")
     res.write("<body><h1>Welcome to Node Js project</h1></body>")
     res.write("</html>")
     return res.end() 
-
+    
 })
 server.listen(4000)
 
