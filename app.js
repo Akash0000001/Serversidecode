f5007ad5d07b960003c593df2d4b73c252049dbe
Akash0000001 <akashranjan947@gmail.com>
2023-05-26 21:32:20 +0530

    redirecting requests

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const http=require("http")
+const fs=require("fs")
 const server=http.createServer((req,res)=>{
     const url=req.url
-    if(url==="/Home")
+    if(url==="/")
     {
         res.setHeader("Content-Type","text/html");
         res.write("<html>")
-        res.write("<head><title>Homepage</title></head>")
-        res.write("<body><h1>Welcome home</h1></body>")
+        res.write("<head><title>Message</title></head>")
+        res.write("<body>")
+        res.write("<h1>Enter Message:</h1>")
+        res.write("<form action=/message method='POST'><input type='text' name='message'></input><input type='submit' value='Send'></input></form>")
+        res.write("</body>")
         res.write("</html>")
-        res.end()
+        return res.end()
+    }
+    else if(url==="/message")
+    {
+        fs.writeFileSync("message.txt","Dummy");
+        res.statusCode=302;
+        res.setHeader("location","/")
+        return res.end();
     }
     else if(url==="/about")
     {
@@ -17,7 +28,7 @@ const server=http.createServer((req,res)=>{
         res.write("<head><title>About us</title></head>")
         res.write("<body><h1>Welcome to About Us page</h1></body>")
         res.write("</html>")
-        res.end()
+        return res.end()
     }
     else if(url==="/node")
     {
@@ -26,8 +37,15 @@ const server=http.createServer((req,res)=>{
         res.write("<head><title>Project</title></head>")
         res.write("<body><h1>Welcome to Node Js project</h1></body>")
         res.write("</html>")
-        res.end() 
+        return res.end() 
     }
+    res.setHeader("Content-Type","text/html");
+    res.write("<html>")
+    res.write("<head><title>Project</title></head>")
+    res.write("<body><h1>Welcome to Node Js project</h1></body>")
+    res.write("</html>")
+    return res.end() 
 
 })
 server.listen(4000)
+
