2b61bec8d242727712132a64acbc7b10c29766a1
Akash0000001 <akashranjan947@gmail.com>
2023-05-28 02:44:30 +0530

    using body-parser to parse the income data

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express= require ("express")
+const bodyparser= require("body-parser")
 
 const app= express()
-
-app.use((req,res,next)=>{
-    console.log("In the middleware")
-    next()
+app.use(bodyparser.urlencoded({extended:false}))
+app.use("/add-product",(req,res,next)=>{
+    res.send("<form action='/product' method='post'><label>Enter product name:</label><input type='text' name='product'></input><label>Number of products:</label><input type='number' name='size'></input><button type='submit'>Send</button></form>")
 })
-app.use((req,res,next)=>{
-    console.log("In another middleware");
-    res.send('<h1> hello to node js </h1>');
+
+app.use("/product",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
+})
+app.use("/",(req,res,next)=>{
+    res.send("<h1>Weclome to node js</h1>");
 })
 
 app.listen(3000)
